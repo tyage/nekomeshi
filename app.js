7fedcea52a984fbf756b314110221a63dadf6459
tyage <tyage@tyage-System-Product-Name.(none)>
2012-08-07 00:15:57 +0900

    port

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-var express = require('express');
-var io = require('socket.io');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var io = require('socket.io').listen(port);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
