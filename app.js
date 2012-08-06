2d6130d1db99140953a6a207822c9f884880b5ed
tyage <tyage@tyage-System-Product-Name.(none)>
2012-08-07 00:36:03 +0900

    chat

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-var port = process.env.PORT || 5000;
-var io = require('socket.io').listen(port);
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io');
+var port = process.env.PORT || 3000;
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+app.configure(function() {
+app.get('/', function(req, res) {
+	console.log('/');
+	res.render('index.html');
+});
+app.listen(port);
+
+var socket = io.listen(app);
+socket.on('connection', function(client) {
+	client.on('message', function(msg) {
+		console.log('send :' + msg);
+		client.send(msg);
+		client.broadcast(msg);
+	});
+	client.on('disconnect', function() {
+		console.log('disconnect');
+	});
 });
 
